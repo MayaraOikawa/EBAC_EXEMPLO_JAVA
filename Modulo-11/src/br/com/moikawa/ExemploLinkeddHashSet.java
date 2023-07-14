f90239ab967da48dd89c3a45e5e1a4c6eb6d9ccc
Mayara Vasconcelos Oikawa <102560227+MayaraOikawa@users.noreply.github.com>
2023-07-14 16:59:59 -0300

    Update ExemploLinkeddHashSet.java

--- Modulo-11/src/br/com/moikawa/ExemploLinkeddHashSet.java
+++ Modulo-11/src/br/com/moikawa/ExemploLinkeddHashSet.java
@@ -1,4 +1,20 @@
 package br.com.moikawa;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class ExemploLinkeddHashSet {
+1
+    public static void main(String args[]) { exemploListaSimples(); }
+
+    private static void exemploListaSimples() {
+        System.out.println(" ***** exemploListaSimples *****");
+        Set<String> lista = new LinkedHashSet<~>();
+        lista.add("Joao da Silva");
+        lista.add("Antonio Souza");
+        lista.add("Lucia Ferreira");
+        System.out.println(lista);
+        System.out.println("");
+    }
 }
